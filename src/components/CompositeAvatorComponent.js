5f5d48454cd93b1e6869355679b311a57d183f42
谢佳杰 <1402601193@qq.com>
2018-11-27 17:31:25 +0800

    优化

--- src/components/CompositeAvatorComponent.js
+++ src/components/CompositeAvatorComponent.js
@@ -1,25 +1,31 @@
 import React from 'react'
 
+//组合头像组件
+
+//头像图片组件
 const ProfilePic = (props) => {
     return (
-        <img style={{width:200,height:200,border:"1px solid #ddd",borderRadius:'50%'}} src={'/images/' + props.src + '.jpg'}/>
+        <img alt={props.name} style={{width: 200, height: 200, border: "1px solid #ddd", borderRadius: '50%'}}
+             src={'/images/' + props.src + '.jpg'}/>
     );
 }
 
+//连接组件
 const ProfileLink = (props) => {
     return (
-        <a style={{textDecoration:"none"}} href={'http://p4.so.qhmsg.com/t01405f384d8c12b9f9.jpg'}>
+        <a style={{textDecoration: "none"}} href={'http://p4.so.qhmsg.com/t01405f384d8c12b9f9.jpg'}>
             {props.name}
         </a>
     );
 }
 
+//头像
 class CompositeAvatorComponent extends React.Component {
     constructor(props) {
         super(props);
         let results = []
         for (let i = 1; i < 5; i++) {
-            results.push({src: "" + i, name: "admin" + i})
+            results.push({id: i, src: "" + i, name: "admin" + i})
         }
         this.state = {results: results};
     }
@@ -29,11 +35,13 @@ class CompositeAvatorComponent extends React.Component {
         return (
             <div>
                 {this.state.results.map((e) => {
-                    return <div key={e.id}>
-                        <ProfilePic style={{display:"block"}} src={e.src}/>
-                        <br/>
-                        <ProfileLink style={{display:"block"}} name={e.name}/>
-                    </div>
+                    return (
+                        <div key={e.id} data-name={e.name}>
+                            <ProfilePic style={{display: "block"}} src={e.src} name={e.name}/>
+                            <br/>
+                            <ProfileLink style={{display: "block"}} name={e.name}/>
+                        </div>
+                    )
                 })}
             </div>
         )
